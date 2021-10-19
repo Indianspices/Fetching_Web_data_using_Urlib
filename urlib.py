baa88aef9ff6267761e6db816e6fde57f4a6840e
Aman India <69667533+amanindiamuzz@users.noreply.github.com>
2021-10-19 10:51:27 +0530

    Updated more liabrary
    Done

--- urlib.py
+++ urlib.py
@@ -1,6 +1,7 @@
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
+import numpty as np
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
@@ -15,3 +16,4 @@ soup = BeautifulSoup(html, 'html.parser')
 tags = soup('span class="comments"')
 for tag in tags:
     print(tag)
+    
