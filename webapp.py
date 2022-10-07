38523bf84c85d4061423dc60f40c865c40fc4b7b
Aman India <69667533+mramanindia@users.noreply.github.com>
2022-10-07 12:38:35 +0530

    Updates the file

--- webapp.py
+++ webapp.py
@@ -1,12 +1,15 @@
+#importing required library
 import socket
 import re
 import numpy
 
+#Building socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
 cmd = 'POST http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
+#Using loop with 512 socket
 while True:
     data = mysock.recv(512)
     if len(data) <0 or 1:
